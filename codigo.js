72d2dcc43f7602b542b7506995a9bf828862b706
Juan Patuna <juanpatuna98@gmail.com>
2024-02-01 23:45:13 -0300

    Portafolio

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let nombre = prompt('Cual es tu nombre?');
+if (nombre.toLowerCase() == "kati" || nombre.toLowerCase() == "katia" || nombre.toLowerCase() == "popus" || nombre.toLowerCase() == "juan"){
+    alert(`Hola ${nombre} Que agradable tu visita!!!`)
+} else {
+    alert(`Hola Sujeto, ojala tengas una agradable experiencia ${nombre}`)
+}
